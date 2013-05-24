6fbdbea59c0afa14a6a6e2dea2b4e283ffd148a1
Takuya Nishimoto <nishimotz@gmail.com>
2013-05-24 10:35:00 +0900

    use add-on template

---
+++ buildVars.py
@@ -0,0 +1,37 @@
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# add-on Name
+	"addon-name" : "focusHighlight",
+	# Add-on description
+	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
+	"addon-summary" : _("Focus Highlight"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on installation and add-on information
+	"addon-description" : _("""Highlight the focused location."""),
+	# version
+	"addon-version" : "0.0.1",
+	# Author(s)
+	"addon-author" : "Takuya Nishimoto <nishimotz@gmail.com>",
+	# URL for the add-on documentation support
+	"addon-url" : None
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = [os.path.join("addon", "globalPlugins", "*.py")]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
