e143bfffad1062ba167bb5aad761199cabb33624
Takuya Nishimoto <nishimotz@gmail.com>
2023-03-03 10:23:23 +0900

    update addon template

--- buildVars.py
+++ buildVars.py
@@ -3,8 +3,15 @@
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-# Full getext (please don't change)
-_ = lambda x : x
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# which returns whatever is given to it as an argument.
+def _(arg):
+	return arg
+
 
 # Add-on information variables
 addon_info = {
@@ -21,7 +28,7 @@ addon_info = {
 	# version
 	"addon_version" : "6.6",
 	# Author(s)
-	"addon_author" : u"Takuya Nishimoto <nishimotz@gmail.com>",
+	"addon_author" : "Takuya Nishimoto <nishimotz@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url" : "https://github.com/nvdajp/focushighlight",
 	# Documentation file name
@@ -32,14 +39,21 @@ addon_info = {
 	"addon_lastTestedNVDAVersion" : "2023.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
+	# Add-on license such as GPL 2
+	"addon_license": None,
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": None,
 }
 
-
-import os.path
-
 # Define the python files that are the sources of your add-on.
-# You can use glob expressions here, they will be expanded.
-pythonSources = [os.path.join("addon", "globalPlugins", "*.py")]
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+# pythonSources = ["addon/globalPlugins/*.py"]
+# For more information on SCons Glob expressions please take a look at:
+# https://scons.org/doc/production/HTML/scons-user/apd.html
+pythonSources = []
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
@@ -47,3 +61,15 @@ i18nSources = pythonSources + ["buildVars.py"]
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
+
+# Base language for the NVDA add-on
+# If your add-on is written in a language other than english, modify this variable.
+# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
+baseLanguage = "en"
+
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions = []
